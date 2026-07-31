ddfcd1203aaf022c1a366cd14b665eccd379aadf
NegiSushant <sushantsinghnegi1998@gmail.com>
2026-07-31 16:05:11 +0530

    auth impelementing

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  env: {
+    DATABASE_URL: process.env.DATABASE_URL,
+    JWT_ACCESS_SECRET: process.env.JWT_ACCESS_SECRET,
+    JWT_REFRESH_SECRET: process.env.JWT_REFRESH_SECRET,
+  },
   reactCompiler: true,
 };
 
